bde090fb4e6dba2bca1f09505187a88a863ca71b
Justin Moon <mail@justinmoon.com>
2025-03-30 19:13:43 +0100

    build: Upgrade Android toolchain
    Also:
    - `just run-emulator` auto-installs emulator if none exists.
    - Set `$ANDROID_AVD_HOME`
    - Create `scripts/run-emulator.sh` and `scripts/create-emulator.sh`

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "{{android-bundle-id}}"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "{{android-bundle-id}}"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -30,23 +30,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+    kotlinOptions { jvmTarget = "17" }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+
 }
 
 dependencies {
@@ -54,7 +45,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -62,7 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -73,7 +64,6 @@ dependencies {
 
     // Jetpack compose / flow
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.0.5")
 
 }
--- flake.nix
+++ flake.nix
@@ -29,19 +29,18 @@
           with sdkPkgs; [
             # Essential build tools
             cmdline-tools-latest
-            # build-tools-34-0-0
-            build-tools-33-0-1 # FIXME: why does it want this?
+            build-tools-35-0-0
             platform-tools
 
             # Platform & API level
-            platforms-android-34
+            platforms-android-35
 
             # NDK for native code compilation
             ndk-28-0-13004108
 
             # Emulator for testing
             emulator
-            system-images-android-34-google-apis-arm64-v8a
+            system-images-android-35-google-apis-arm64-v8a
           ]);
       in {
         default = pkgs.mkShell {
@@ -59,6 +58,9 @@
             export ANDROID_HOME=${androidSdk}/share/android-sdk
             export ANDROID_SDK_ROOT=${androidSdk}/share/android-sdk
             export ANDROID_NDK_ROOT=${androidSdk}/share/android-sdk/ndk/28.0.13004108
+            # this will work with the `just create-emulator` command, but probably a better way to do this ...
+            export ANDROID_AVD_HOME=$PWD/android-avd
+
             export JAVA_HOME=${pkgs.jdk17.home}
             export PATH=$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 
